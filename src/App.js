13cf5798bf8cbdddefd4370c8067c0667925922a
avinash.adluri@bugcrowd.com <avinash.adluri@bugcrowd.com>
2021-05-24 15:08:38 +1000

    init

---
+++ src/App.js
@@ -0,0 +1,117 @@
+import { useState, useEffect } from "react";
+import { Button, Form, Container, Header } from "semantic-ui-react";
+import axios from "axios";
+import TableData from "./Table";
+import "./App.css";
+
+function App() {
+  const [Name, setName] = useState("");
+  const [Contact, setContact] = useState("");
+  const [BusinessName, setBusinessName] = useState("");
+  const [TypeOfService, setTypeOfService] = useState("");
+  const [LastUsed, setLastUsed] = useState("");
+  const [CummulativeCount, setCummulativeCount] = useState("");
+  const [AddedBy, setAddedBy] = useState("");
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const API =
+    "https://sheet.best/api/sheets/7e4c31b8-5cb4-4fab-9fea-a288753da5d1";
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const body = {
+      Name,
+      Contact,
+      BusinessName,
+      TypeOfService,
+      LastUsed,
+      CummulativeCount,
+      AddedBy,
+    };
+
+    axios.post(API, body).then((response) => {
+      setData([...data, response.data[0]]);
+      console.log(response.data[0]);
+    });
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await axios.get(API).then((response) => {
+        setData(response.data);
+      });
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if (loading) return <h2>Loading....</h2>;
+
+  return (
+    <Container fluid className="container">
+      <Header as="h2">Monarco vendor data</Header>
+      <Form className="form">
+        <Form.Field>
+          <label>Name</label>
+          <input
+            placeholder="Enter your Name"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Contact</label>
+          <input
+            placeholder="Enter Contact"
+            onChange={(e) => setContact(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Business Name</label>
+          <input
+            placeholder="Enter Business Name"
+            onChange={(e) => setBusinessName(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Type Of Service</label>
+          <input
+            placeholder="Enter Type Of Service"
+            onChange={(e) => setTypeOfService(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Last Used</label>
+          <input
+            placeholder="Enter Last Used"
+            onChange={(e) => setLastUsed(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Cummulative Count</label>
+          <input
+            placeholder="Enter Cummulative Count"
+            onChange={(e) => setCummulativeCount(e.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Added By</label>
+          <input
+            placeholder="Enter Added By (Avinash, 510, D ...)"
+            onChange={(e) => setAddedBy(e.target.value)}
+          />
+        </Form.Field>
+        <div className="navigation">
+          <Button color="blue" type="submit" onClick={handleSubmit}>
+            Submit
+          </Button>
+        </div>
+      </Form>
+      <hr />
+      <TableData data={data} />
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/Table.js
@@ -0,0 +1,35 @@
+import { Table } from "semantic-ui-react";
+
+const TableData = ({ data }) => {
+  return (
+    <Table>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>Name</Table.HeaderCell>
+          <Table.HeaderCell>Contact</Table.HeaderCell>
+          <Table.HeaderCell>BusinessName</Table.HeaderCell>
+          <Table.HeaderCell>TypeOfService</Table.HeaderCell>
+          <Table.HeaderCell>LastUsed</Table.HeaderCell>
+          <Table.HeaderCell>CummulativeCount</Table.HeaderCell>
+          <Table.HeaderCell>AddedBy</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+
+      <Table.Body>
+        {data?.map((row, i) => (
+          <Table.Row key={i}>
+            <Table.Cell>{row.Name}</Table.Cell>
+            <Table.Cell>{row.Contact}</Table.Cell>
+            <Table.Cell>{row.BusinessName}</Table.Cell>
+            <Table.Cell>{row.TypeOfService}</Table.Cell>
+            <Table.Cell>{row.LastUsed}</Table.Cell>
+            <Table.Cell>{row.CummulativeCount}</Table.Cell>
+            <Table.Cell>{row.AddedBy}</Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table>
+  );
+};
+
+export default TableData;
